d4d768d06f359e5bc698888819349d79d8ece871
Vadim Zhavoronkov <poshash@live.ru>
2018-07-27 18:17:08 +1000

    Create gamefield
    Gamefield with visible tiles was created

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from view import *
+from tiles import *
 
+import random
 pygame.init()
 
 SCREEN_RESOLUTION = 1200, 800
 
 DISPLAY = pygame.display.set_mode(SCREEN_RESOLUTION)
 pygame.display.set_caption('Mahjong')
+tiles_list = tiles_setup()
 
 
 def main():
+    tiles = create_gamefield()
+
     menu_on = True
     menu = Menu()
     tiles_left = 144
@@ -20,7 +25,9 @@ def main():
             start_time = pygame.time.get_ticks()
             menu.draw(DISPLAY)
         else:
-            left_str = 'Tiles left: ' + str(tiles_left) + '/144';
+            draw_tiles(tiles, DISPLAY)
+
+            left_str = 'Tiles left: ' + str(tiles_left) + '/144'
             tiles_left_text = Text(left_str, TEXT_COLOR, (1000, 100))
             tiles_left_text.draw(DISPLAY)
             time_text = show_time(time_passed)
@@ -31,14 +38,37 @@ def main():
             shuffle_button = Button(MENU_BACKGROUND, 500, 100, 'SHUFFLE', TEXT_COLOR, 42, (1000, 400))
             shuffle_button.draw(DISPLAY)
         for event in pygame.event.get():
-            if event.type == 12: #QUIT
+            if event.type == 12:  # QUIT
                 pygame.quit()
                 sys.exit()
-            elif event.type == 6: #MOUSE_UP
+            elif event.type == 6:  # MOUSE_UP
                 mouse_pos = event.pos
                 if menu_on:
                     menu_on = menu.action(mouse_pos)
         pygame.display.update()
+
+
+def draw_tiles(tiles, surface):
+    for tile in tiles:
+        surface.blit(tile.image, tile.rect)
+
+
+def create_gamefield():
+    local_tiles = []
+    global tiles_list
+
+    for i in range(0, 7):
+        for j in range(0, 14):
+            r_index = random.randrange(len(tiles_list))
+            tile = tiles_list.pop(r_index)
+
+            tile.rect.x = tile.rect.width + j * tile.rect.width
+            tile.rect.y = tile.rect.height + i * tile.rect.height
+            local_tiles.append(tile)
+
+    tiles_list = tiles_setup()
+
+    return local_tiles
 
 
 if __name__ == '__main__':
--- tiles.py
+++ tiles.py
@@ -1,13 +1,14 @@
 import pygame
 
 
-class Tile (pygame.sprite.Sprite):
+class TTile (pygame.sprite.Sprite):
     def __init__(self, file):
         super().__init__()
         self.name = file.rstrip('.png')
         self.image = pygame.image.load('img/' + file).convert()
         self.rect = self.image.get_rect()
         self.clicked = False
+        self.layer = 0
 
     def is_chosen(self):
         return self.clicked
@@ -30,13 +31,34 @@ class Tile (pygame.sprite.Sprite):
 
 
 def tiles_setup():
-    # reading tiles from file here
+    img_list = [
+                 "b1.png", "b2.png", "b3.png", "b4.png", "b5.png", "b6.png", "b7.png", "b8.png", "b9.png",
+                 "c1.png", "c2.png", "c3.png", "c4.png", "c5.png", "c6.png", "c7.png", "c8.png", "c9.png",
+                 "p1.png", "p2.png", "p3.png", "p4.png", "p5.png", "p6.png", "p7.png", "p8.png", "p9.png",
+                 "w1.png", "w2.png", "w3.png", "w4.png", "d1.png", "d2.png", "d3.png",
+                 "f1.png", "f2.png", "f3.png", "f4.png", "s1.png", "s2.png", "s3.png", "s4.png"
+               ]
     tiles_list = []
+
+    for i in range(len(img_list)):
+        for j in range(0, 4):
+            tile = TTile(img_list[i])
+            tiles_list.append(tile)
+
     return tiles_list
 
+
 def shuffle():
+    emptyhere = 1
     # shuffling tiles function
+
 
 def pairs_counting(tiles):
     pairs_dict = {}
     count_pairs = 0
+
+
+
+
+
+
